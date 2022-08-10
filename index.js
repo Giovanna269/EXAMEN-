9416d8cd75ecc3d397ded4aa73f94ddf6a0a62cd
Giovanna269 <110567788+Giovanna269@users.noreply.github.com>
2022-08-10 15:15:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function contactanos(){
+
+    var ingreso1 = Text($("#ingreso1").val());
+
+    var ingreso2 = Text($("#ingreso2").val());
+
+    const result1 = document.getElementById("mensaje1");
+
+    result1.innerText = area1;
+}
